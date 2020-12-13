62d6095e12535b4e3852110e5d57026b529c4dff
Jatin Vaidya <jatin.vaidya@auth0.com>
2020-12-13 14:58:16 +1100

    working

--- README.md
+++ README.md
@@ -17,8 +17,81 @@ Generate real-looking fake user data and load to Auth0
 - AUTH0_TESTUSER_PASSWORD (common password for all test users)
 - AUTH0_CONNECTION (name of auth0 connection to load users to)
 
-4. Execute the following command to start user load:
+4. Execute the `user-loader-v2.js`:
 
-`./user-loader.js --load=<number-of-users-to-load> --flush=<num-users-before-buffer-flushed-to-csv>`
+```
+Usage: user-loader-v2.js --numusers=[num] --concurrent=[num|5] --delay=[num|333]
+--retry=[num|3] --status=[num|60]
 
-5. Check generated `users.csv` file for `email` and `password` for all users that were loaded to Auth0.
+Options:
+  --help        Show help                                              [boolean]
+  --version     Show version number                                    [boolean]
+  --numusers    Number of users to be created                [number] [required]
+  --concurrent  Max concurrent reqs (1..20)                [number] [default: 5]
+  --delay       Min delay (ms) betn reqs (300..3000)     [number] [default: 333]
+  --retry       Num retries for HTTP429 reqs (1..5)        [number] [default: 2]
+  --status      Min status reporting interval (sec)       [number] [default: 60]
+```
+
+5. Check generated `users-list.json` file for `email` and `password` for all users that were loaded to Auth0.
+
+> [{"username":"Dallas.Tromp.n3h@hotmail.com","password":"Mju76yhn"},{"username":"Abigayle.Connelly.Qu2@hotmail.com","password":"Mju76yhn"}]
+
+## Sample Run:
+
+```
+auth0-user-loader % ./user-loader-v2.js --concurrent=3 --delay=500 --numusers=5000
+[2020-05-01T04:36:29.573Z] 
+    Number of users to be created: 5000,
+    Max concurrent requests: 3,
+    Min delay between requests: 500,
+    Number of retry attempts for HTTP 429 failed requests: 2,
+    Min status reporting interval: 60
+
+[2020-05-01T04:36:29.579Z] [truncateFile] file truncated users.csv
+[2020-05-01T04:36:29.579Z] [truncateFile] file truncated users.csv
+[2020-05-01T04:36:29.579Z] acquiring access_token for mgmt-api
+[2020-05-01T04:36:29.684Z] start loading users
+[2020-05-01T04:37:29.814Z] -------------------------
+[2020-05-01T04:37:29.816Z] success so far: 120 failure so far: 0
+[2020-05-01T04:37:29.818Z] -------------------------
+[2020-05-01T04:38:29.817Z] -------------------------
+[2020-05-01T04:38:29.817Z] success so far: 240 failure so far: 0
+[2020-05-01T04:38:29.818Z] -------------------------
+[2020-05-01T04:39:29.819Z] -------------------------
+[2020-05-01T04:39:29.819Z] success so far: 360 failure so far: 0
+[2020-05-01T04:39:29.819Z] -------------------------
+[2020-05-01T04:40:29.824Z] -------------------------
+[2020-05-01T04:40:29.824Z] success so far: 480 failure so far: 0
+[2020-05-01T04:40:29.824Z] -------------------------
+[2020-05-01T04:40:29.825Z] [Noemy.Pfannerstill39@yahoo.com] failed - in spite of max retries
+[2020-05-01T04:41:29.826Z] -------------------------
+[2020-05-01T04:41:29.826Z] success so far: 600 failure so far: 1
+[2020-05-01T04:41:29.826Z] -------------------------
+.
+.
+.
+[2020-05-01T04:42:29.829Z] -------------------------
+[2020-05-01T04:42:29.829Z] success so far: 720 failure so far: 5
+[2020-05-01T04:42:29.830Z] -------------------------
+[2020-05-01T04:43:29.831Z] -------------------------
+[2020-05-01T04:43:29.831Z] success so far: 840 failure so far: 5
+[2020-05-01T04:43:29.831Z] -------------------------
+.
+.
+.
+[2020-05-01T05:15:29.932Z] -------------------------
+[2020-05-01T05:15:29.932Z] success so far: 4670 failure so far: 5
+[2020-05-01T05:15:29.933Z] -------------------------
+[2020-05-01T05:16:29.935Z] -------------------------
+[2020-05-01T05:16:29.935Z] success so far: 4790 failure so far: 5
+[2020-05-01T05:16:29.935Z] -------------------------
+[2020-05-01T05:17:29.940Z] -------------------------
+[2020-05-01T05:17:29.940Z] success so far: 4910 failure so far: 5
+[2020-05-01T05:17:29.940Z] -------------------------
+[2020-05-01T05:18:06.964Z] *****************************
+[2020-05-01T05:18:06.964Z] final success: 4985 final failure: 5
+[2020-05-01T05:18:06.964Z] *****************************
+```
+
+
--- user-loader.js
+++ user-loader.js
@@ -1,24 +1,113 @@
 #!/usr/bin/env node
 
+// requires
 const faker = require('faker')
 const dotenv = require('dotenv')
 const request = require('request-promise')
-const argv = require('yargs').argv
-const stringify = require('csv-stringify')
+const argv = require('yargs').options({
+    numusers: { type: 'number', describe: 'Number of users to be created', demandOption: true },
+    concurrent: { default: 5, type: 'number', describe: 'Max concurrent reqs (1..20)' },
+    delay: { default: 333, type: 'number', describe: 'Min delay (ms) betn reqs (300..3000)' },
+    retry: { default: 2, type: 'number', describe: 'Num retries for HTTP429 reqs (1..5)' },
+    status: { default: 60, type: 'number', describe: 'Min status reporting interval (sec)' }
+}).usage('Usage: $0 --numusers=[num] --concurrent=[num|5] --delay=[num|333] --retry=[num|3] --status=[num|60]').argv;
 const fs = require('fs');
+const os = require('os');
+const bottleneck = require('bottleneck');
+const validator = require('node-input-validator');
+const randomString = require('randomstring');
+const { time } = require('console');
+require('log-timestamp')
 
+// env
 dotenv.load();
+
+// cmdline args
 const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
 const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
 const AUTH0_CLIENT_SECRET = process.env.AUTH0_CLIENT_SECRET;
 const AUTH0_CONNECTION = process.env.AUTH0_CONNECTION;
 const AUTH0_TESTUSER_PASSWORD = process.env.AUTH0_TESTUSER_PASSWORD;
-const NUM_USERS = argv.load;
-const MAX_BUFFER = argv.flush;
-var USER_ARR = new Array();
+const BATCH_SIZE = process.env.BATCH_SIZE;
+
+// cmdline args
+const NUM_USERS = argv.numusers;
+const MAX_CONCURRENT_REQUESTS = argv.concurrent;
+const MIN_DELAY_REQUESTS = argv.delay;
+const RETRY_COUNT = argv.retry;
+const STATUS_INTERVAL = argv.status;
+
+// just summary of inputs/defaults used
+console.debug(`
+    Number of users to be created: ${NUM_USERS},
+    Max concurrent requests: ${MAX_CONCURRENT_REQUESTS},
+    Min delay between requests: ${MIN_DELAY_REQUESTS},
+    Number of retry attempts for HTTP 429 failed requests: ${RETRY_COUNT},
+    Min status reporting interval: ${STATUS_INTERVAL}
+`);
+
+// input valiations
+let inputValidator = async () => {
+
+    // input valiation check
+    let concurrent = new validator({ concurrent: MAX_CONCURRENT_REQUESTS }, { concurrent: 'required|integer' });
+    let retry = new validator({ retry: RETRY_COUNT }, { retry: 'required|between:0,5' });
+    let delay = new validator({ delay: MIN_DELAY_REQUESTS }, { delay: 'required|between:300,3000' });
+    let status = new validator({ status: STATUS_INTERVAL }, { status: 'required|between:5,3600' });
+    let checks = [concurrent, retry, delay, status];
+
+    Promise
+        .all(checks.map(check => check.check()))
+        .then(values => {
+            if (values.includes(false)) {
+                console.error(checks[values.indexOf(false)].errors);
+                console.error('see usage: ./user-loader.js --help');
+                process.exit(1);
+            }
+        }).catch(error => {
+            console.error('error performing validation check', error.message);
+            process.exit(1);
+        });
+}
+
+
+// globals
+var ACCESS_TOKEN = '';
+const CSV_OUT = 'users.csv';
+const JSON_OUT = `users-list_${Date.now()}.json`;
+const FAILURE_LOG = 'failures.log'
+
+// rate limit sending of requests
+var limiter = new bottleneck({
+    maxConcurrent: MAX_CONCURRENT_REQUESTS,
+    minTime: MIN_DELAY_REQUESTS
+});
+
+// listen to "failed" event, then retry
+limiter.on("failed", (error, jobInfo) => {
+    const id = jobInfo.options.id;
+
+    if (error.statusCode === 429) {
+        if (jobInfo.retryCount < RETRY_COUNT) { // max-retry-attempts
+            // console.warn(`[${id}] failed - will be retried in ${MIN_DELAY_REQUESTS} ms!`);
+            return MIN_DELAY_REQUESTS;
+        } else {
+            console.error(`[${id}] failed - in spite of max retries`)
+            appendToFile(FAILURE_LOG, `${id},${error.statusCode}`)
+        }
+    } else {
+        console.error(`[${id}] failed - will NOT be retried as error isn't 429 but ${error.statusCode}`)
+        appendToFile(FAILURE_LOG, `${id},${error.statusCode}`)
+    }
+});
+
+// listen to the "retry" event
+limiter.on("retry", (error, jobInfo) => {
+    // console.debug(`Now retrying ${jobInfo.options.id}`)
+});
 
 // mgmt api access_token
-async function acquireAccessToken() {
+let acquireAccessToken = async () => {
     console.log('acquiring access_token for mgmt-api');
     var options = {
         method: 'POST',
@@ -35,117 +124,144 @@ async function acquireAccessToken() {
         }`
     };
 
-    const responseBody = await request(options, function (error, response, body) {
+    const responseBody = await request(options, error => {
         if (error) throw new Error(error);
     });
 
-    return JSON.parse(responseBody).access_token;
+    ACCESS_TOKEN = JSON.parse(responseBody).access_token;
 }
 
 // generate fake data for one user
 // return user
-function getFakeUserData() {
+let getFakeUserData = () => {
     var email = faker.internet.email();
+    // faker generates duplicates many times, so we append 5 random chars
+    email = `${email.split('@')[0]}.${randomString.generate(5)}@${email.split('@')[1]}`;
     var password = AUTH0_TESTUSER_PASSWORD
-    return {
-        email,
-        password
-    };
+    var fullname = faker.fake("{{name.lastName}}, {{name.firstName}}")
+    var nickname = fullname.split(',')[1].trim()
+    return { email, password, fullname, nickname };
 }
 
 // create one user on auth0
 // return status code
-async function createUserOnAuth0(user, accessToken) {
+let createUserOnAuth0 = (user) => {
     var options = {
         method: 'POST',
         url: `https://${AUTH0_DOMAIN}/api/v2/users`,
         headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${accessToken}`
+            'Content-Type': 'application/json'
         },
+        auth: { 'bearer': ACCESS_TOKEN },
         body: {
             connection: AUTH0_CONNECTION,
             email: user.email,
             password: user.password,
-            email_verified: true
+            email_verified: true,
+            user_metadata: {
+                fullName: user.fullname,
+                nickName: user.nickname,
+                locale: 'en-US'
+            },
+            app_metadata: {
+                useMfa: false
+            }
         },
-        json: true,
-        resolveWithFullResponse: true
+        json: true
     };
 
-    const response = await request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-    });
-    console.log(`user created: ${response.body.email}`);
-    return response.statusCode;
-}
-
-// stringify array to csv and flush to csv-file
-// return promise
-function flushUsersToCsv() {
-    console.log(`--------flush to csv: ${USER_ARR.length}---------`);
-    return new Promise((resolve, reject) => {
-        stringify(USER_ARR, (err, output) => {
-            if (err) reject(err);
-            else {
-                writeToFileAsPromised('users.csv', output)
-                    .then(USER_ARR = new Array())
-                    .then(resolve());
-            }
-        });
-    });
+    return limiter.schedule({ id: user.email }, request, options);
 }
 
 // write to csv-file
-// return promise
-function writeToFileAsPromised(path, data) {
-    return new Promise((resolve, reject) => {
-        fs.appendFile('users.csv', data, (error) => {
-            if (error) reject(error);
-            else resolve();
-        });
+let appendToFile = (path, data) => {
+    fs.appendFile(path, data + os.EOL, (error) => {
+        if (error) console.error('[appendToFile] data', data, 'error', error);
     });
 }
 
-// truncate csv file
-// return promise
-function truncateFileAsPromised(path) {
+// truncate existing file
+let truncateFile = (path) => {
     return new Promise((resolve, reject) => {
-        fs.truncate('users.csv', 0, (error) => {
-            if (error) reject(error);
-            else {
-                console.log('truncated users.csv')
+        fs.truncate(path, 0, (error) => {
+            if (error) {
+                reject(error);
+            } else {
+                console.debug('[truncateFile] file truncated', path)
                 resolve();
             }
         });
     });
 }
 
-// main
-async function main() {
+let main = async () => {
 
-    // truncate csv-file
-    await truncateFileAsPromised();
+    await Promise.all([
+        truncateFile(JSON_OUT).catch(error => {
+            // it's ok, 'users.csv' probably did not exist
+        }),
+        truncateFile(FAILURE_LOG).catch(error => {
+            // it's ok, 'failures.log' probably did not exist
+        })
+    ]);
 
-    // fetch access_token for creating user
-    const accessToken = await acquireAccessToken();
-    
-    // create N users
-    for (i = 0; i < NUM_USERS; i++) {
-        user = getFakeUserData();
-        const statusCode = await createUserOnAuth0(user, accessToken);
+    // acquire access token for create-user mgmt-api calls
+    await acquireAccessToken().catch(error => {
+        console.error('[main] acquireAccessToken error, cannot continue', error.message);
+        process.exit(1);
+    });
 
-        // if creation successful, push to array
-        if (statusCode === 201)
-            USER_ARR.push(user);
+    console.info('start loading users');
+    let success = 0; let failure = 0;
 
-        // time to flush array to csv-file    
-        if (USER_ARR.length >= MAX_BUFFER)
-            await flushUsersToCsv();
+    let jsonOut = new Array();
+    for (j = 0; j < Math.floor(NUM_USERS / BATCH_SIZE); j++) {
+        let promises = new Array();
+        for (i = 0; i < BATCH_SIZE; i++) {
+            let user = getFakeUserData();
+            promises.push(
+                createUserOnAuth0(user)
+                    .then(response => {
+                        jsonOut.push({
+                            user_id: response.user_id,
+                            username: user.email
+                        });
+                        success++;
+                    }).catch(error => {
+                        console.error('user', user.email, 'error', error.message);
+                        failure++;
+                    })
+            );
+        }
+        await Promise.all(promises);
+        appendToFile(JSON_OUT, `${JSON.stringify(jsonOut)}`);
+        jsonOut = new Array();
+        console.info('success so far:', success, 'failure so far:', failure);
     }
-    // flush leftovers to csv file
-    if (USER_ARR.length > 0)
-        flushUsersToCsv();
+
+    let promises = new Array();
+    for (i = 0; i < NUM_USERS % BATCH_SIZE; i++) {
+        let user = getFakeUserData();
+        promises.push(
+            createUserOnAuth0(user)
+                .then(response => {
+                    jsonOut.push({
+                        user_id: response.user_id,
+                        username: user.email
+                    });
+                    success++;
+                }).catch(error => {
+                    console.error('user', user.email, 'error', error.message);
+                    failure++;
+                })
+        );
+    }
+    await Promise.all(promises);
+    appendToFile(JSON_OUT, `${JSON.stringify(jsonOut)}`);
+
+    console.info('*****************************');
+    console.info('final success:', success, 'final failure:', failure);
+    console.info('*****************************');
 }
 
 // execute
